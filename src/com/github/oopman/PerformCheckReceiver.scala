88ab02ad6571d65f802c25e3fb867e0af400d4e2
Adam Jorgensen <adam.jorgensen.za@gmail.com>
2015-03-14 15:44:19 +0200

    Extremely basic initial implementation of VolumeLabel2Symlink.

---
+++ src/com/github/oopman/PerformCheckReceiver.scala
@@ -0,0 +1,9 @@
+package com.github.oopman
+
+import android.content.{Intent, Context, BroadcastReceiver}
+
+class PerformCheckReceiver extends BroadcastReceiver {
+  override def onReceive(context: Context, intent: Intent): Unit = {
+    //TODO: Use SuperSU to perform check
+  }
+}
--- src/com/github/oopman/VolumeLabel2Symlink.scala
+++ src/com/github/oopman/VolumeLabel2Symlink.scala
@@ -1,18 +1,55 @@
 package com.github.oopman
 
+import android.app.{PendingIntent, AlarmManager}
+import android.content.Intent
 import android.graphics.Color
+import android.os.AsyncTask
+import android.view.View
+import eu.chainfire.libsuperuser.Shell
 import org.scaloid.common._
+//import com.github.oopman.PerformCheckReceiver
+import scala.collection.JavaConverters._
+
+import scala.concurrent.{ExecutionContext, Future}
 
 class VolumeLabel2Symlink extends SActivity {
+  implicit val exec = ExecutionContext.fromExecutor(
+    AsyncTask.THREAD_POOL_EXECUTOR)
+
 
   onCreate {
+    val labelledBlockDevicePattern = "LABEL=\"([^\"]+)\"".r.unanchored
+    val blockDevicePattern = "/dev/block/([^:]+)".r.unanchored
+//    val mountEntryPattern = "/mnt/usb/([\\S]+)".r.unanchored
+
     contentView = new SVerticalLayout {
       style {
         case t: STextView => t textSize 10.dip
       }
       STextView("VolumeLabel2Symlink").textSize(20.dip).marginBottom(20.dip)
-      STextView("The app is now monitoring for attached block devices with labels. Symlinks will be created under /mnt/labelled")
+      STextView("Click the button to trigger a check for labelled block devices. Symlinks will be created under /mnt/usb")
+      SButton("Scan and Label", (view: View) => {
+        Future {
+          alert("Results",
+            (for (resultLine <- Shell.SU.run("blkid").asScala) yield (resultLine, resultLine) match {
+              case (blockDevicePattern(device), labelledBlockDevicePattern(label)) =>
+                Shell.SU.run(s"ln -s /mnt/usb/$device /mnt/usb/$label")
+                s"Linked /mnt/usb/$device to /mnt/usb/$label"
+              case _ => ""
+            }) filter {
+              _.length > 0
+            } mkString "\n")
+        }
+      })
     } padding 20.dip
+
+    /*
+    alarmManager.setRepeating(
+      AlarmManager.RTC_WAKEUP,
+      System.currentTimeMillis(),
+      1000,
+      PendingIntent.getBroadcast(ctx, 0, SIntent[PerformCheckReceiver], 0))
+    */
   }
 
 }
