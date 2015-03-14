681cb8c061b0e762c726e8cdcfa408796943fc76
Adam Jorgensen <adam.jorgensen.za@gmail.com>
2015-03-14 08:31:31 +0200

    Removed example hello-scaloid code from activity.

--- src/com/github/oopman/VolumeLabel2Symlink.scala
+++ src/com/github/oopman/VolumeLabel2Symlink.scala
@@ -8,18 +8,10 @@ class VolumeLabel2Symlink extends SActivity {
   onCreate {
     contentView = new SVerticalLayout {
       style {
-        case b: SButton => b.textColor(Color.RED).onClick(toast("Bang!"))
         case t: STextView => t textSize 10.dip
-        case e: SEditText => e.backgroundColor(Color.YELLOW)
       }
-      STextView("I am 10 dip tall")
-      STextView("Me too")
-      STextView("I am 15 dip tall") textSize 15.dip // overriding
-      this += new SLinearLayout {
-        STextView("Button: ")
-        SButton("Something")
-      }.wrap
-      SEditText("Yellow input field fills the space").fill
+      STextView("VolumeLabel2Symlink").textSize(20.dip).marginBottom(20.dip)
+      STextView("The app is now monitoring for attached block devices with labels. Symlinks will be created under /mnt/labelled")
     } padding 20.dip
   }
 
