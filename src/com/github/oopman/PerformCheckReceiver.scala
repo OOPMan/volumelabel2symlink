28bd72e889d8bab16dd5591142a3f55fb53b539f
Adam Jorgensen <adam.jorgensen.za@gmail.com>
2015-03-15 17:15:29 +0200

    Removed PerformCheckReceiever.

--- src/com/github/oopman/PerformCheckReceiver.scala
+++
@@ -1,9 +0,0 @@
-package com.github.oopman
-
-import android.content.{Intent, Context, BroadcastReceiver}
-
-class PerformCheckReceiver extends BroadcastReceiver {
-  override def onReceive(context: Context, intent: Intent): Unit = {
-    //TODO: Use SuperSU to perform check
-  }
-}
