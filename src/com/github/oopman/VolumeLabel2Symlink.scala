4eb10baae42b11e8fe62063308242c2630b39d53
Adam Jorgensen <adam.jorgensen.za@gmail.com>
2015-03-18 20:32:20 +0200

    Implemented very basic UI skeleton.

--- src/com/github/oopman/VolumeLabel2Symlink.scala
+++ src/com/github/oopman/VolumeLabel2Symlink.scala
@@ -1,13 +1,11 @@
 package com.github.oopman
 
-import android.app.{PendingIntent, AlarmManager}
 import android.content.Intent
 import android.graphics.Color
 import android.os.AsyncTask
 import android.view.View
 import eu.chainfire.libsuperuser.Shell
 import org.scaloid.common._
-//import com.github.oopman.PerformCheckReceiver
 import scala.collection.JavaConverters._
 
 import scala.concurrent.{ExecutionContext, Future}
@@ -16,11 +14,14 @@ class VolumeLabel2Symlink extends SActivity {
   implicit val exec = ExecutionContext.fromExecutor(
     AsyncTask.THREAD_POOL_EXECUTOR)
 
+  val labelledBlockDevicePattern = "LABEL=\"([^\"]+)\"".r.unanchored
+  val blockDevicePattern = "/dev/block/([^:]+)".r.unanchored
+  //    val mountEntryPattern = "/mnt/usb/([\\S]+)".r.unanchored
+  val defaultLinkLocation = "/mnt/usb"
+  val defaultScanLocations = Set("/mnt/usb")
 
   onCreate {
-    val labelledBlockDevicePattern = "LABEL=\"([^\"]+)\"".r.unanchored
-    val blockDevicePattern = "/dev/block/([^:]+)".r.unanchored
-//    val mountEntryPattern = "/mnt/usb/([\\S]+)".r.unanchored
+    val prefs = getPreferences(0)
 
     contentView = new SVerticalLayout {
       style {
@@ -28,6 +29,17 @@ class VolumeLabel2Symlink extends SActivity {
       }
       STextView("VolumeLabel2Symlink").textSize(20.dip).marginBottom(20.dip)
       STextView("Click the button to trigger a check for labelled block devices. Symlinks will be created under /mnt/usb")
+      SButton("Set Link Location", (view: View) => {
+        //TODO: Display Folder chooser
+      })
+      SButton("Add Scan Location", (view: View) => {
+        //TODO: Display Folder chooser
+      })
+      //TODO: ListAdaptor
+      val scanLocations = SListView().adapter(SArrayAdapter(prefs.getStringSet("scanLocations", defaultScanLocations.asJava)))
+      SButton("Remove Selected Scan Location", (view: View) => {
+        //TODO: Implement
+      })
       SButton("Scan and Label", (view: View) => {
         Future {
           alert("Results",
@@ -42,14 +54,6 @@ class VolumeLabel2Symlink extends SActivity {
         }
       })
     } padding 20.dip
-
-    /*
-    alarmManager.setRepeating(
-      AlarmManager.RTC_WAKEUP,
-      System.currentTimeMillis(),
-      1000,
-      PendingIntent.getBroadcast(ctx, 0, SIntent[PerformCheckReceiver], 0))
-    */
   }
 
 }
