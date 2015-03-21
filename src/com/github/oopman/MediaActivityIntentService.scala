0f6f850d44464f8d6f8ed60eba2fc7b0d2390953
Adam Jorgensen <adam.jorgensen.za@gmail.com>
2015-03-21 21:33:32 +0200

    Implemented Scan and Link logic in MediaActivityIntentService.

--- src/com/github/oopman/MediaActivityIntentService.scala
+++ src/com/github/oopman/MediaActivityIntentService.scala
@@ -3,8 +3,9 @@ package com.github.oopman
 import android.app.IntentService
 import android.content.{Context, Intent}
 import android.os.Bundle
-import eu.chainfire.libsuperuser.Application
+import eu.chainfire.libsuperuser.{Shell, Application}
 import org.scaloid.common.{SContext, SIntent}
+import scala.collection.JavaConverters._
 
 object MediaActivityIntentService {
   val ACTION_MEDIA_MOUNTED = "media_mount"
@@ -20,8 +21,29 @@ object MediaActivityIntentService {
 }
 
 class MediaActivityIntentService extends IntentService("MediaActivityIntentService") with SContext {
-  override def onHandleIntent(intent: Intent): Unit = {
-    Application.toast(this, intent.getAction)
+  override def onHandleIntent(intent: Intent) = {
+    val mountOutput = Shell.SU.run("mount").asScala
+    intent.getAction match {
+      case MediaActivityIntentService.ACTION_MEDIA_MOUNTED =>
+        val prefs = getSharedPreferences("VolumeLabel2Symlink", Context.MODE_PRIVATE)
+        val linkLocation = prefs.getString("linkLocation", defaultLinkLocation)
+        val scanLocations = prefs.getStringSet("scanLocations", defaultScanLocations).asScala
+        //TODO: Ensure link location exists
+        for (scanLocation <- scanLocations; mountPoint <- mountOutput) {
+          val mountEntryPattern = s"(\\S+)\\s+($scanLocation\\S*)\\s+".r.unanchored
+          mountPoint match {
+            case mountEntryPattern(devicePath, mount) =>
+              for (blkidOutputLine <- Shell.SU.run(s"blkid $devicePath").asScala) blkidOutputLine match {
+                case blockDeviceLabelPattern(deviceLabel) =>
+                  Shell.SU.run(s"ln -s \"$mount\" \"$linkLocation/$deviceLabel\"")
+                case _ =>
+              }
+            case _ =>
+          }
+        }
 
+      case MediaActivityIntentService.ACTION_MEDIA_UNMOUNTED =>
+    }
+    Application.toast(this, intent.getAction)
   }
 }
