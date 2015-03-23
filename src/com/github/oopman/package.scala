371454a2e135dfa33263f38219a1686b32ccbd09
Adam Jorgensen <adam.jorgensen.za@gmail.com>
2015-03-23 15:28:43 +0200

    Updated default link location again.

--- src/com/github/oopman/package.scala
+++ src/com/github/oopman/package.scala
@@ -4,6 +4,6 @@ import scala.collection.JavaConverters._
 
 package object oopman {
   val blockDeviceLabelPattern = "LABEL=\"([^\"]+)\"".r.unanchored
-  val defaultLinkLocation = "/mnt/sdcard/links"
+  val defaultLinkLocation = "/mnt/storage/links"
   val defaultScanLocations = Set("/mnt").asJava
 }
