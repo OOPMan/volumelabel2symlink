16c342ef3815fb8b84903a09b0d8edd620198bb7
Adam Jorgensen <adam.jorgensen.za@gmail.com>
2015-03-27 13:30:50 +0200

    Tweaked default locations. Again.

--- src/com/github/oopman/package.scala
+++ src/com/github/oopman/package.scala
@@ -4,6 +4,6 @@ import scala.collection.JavaConverters._
 
 package object oopman {
   val blockDeviceLabelPattern = "LABEL=\"([^\"]+)\"".r.unanchored
-  val defaultLinkLocation = "/mnt/storage/links"
-  val defaultScanLocations = Set("/mnt").asJava
+  val defaultLinkLocation = "/mnt/usb/links"
+  val defaultScanLocations = Set("/mnt/usb").asJava
 }
